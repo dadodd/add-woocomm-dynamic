c4cec8fdf9aa1041fd0ad5a626d49983a3a1df5e
dadodd <draduvnj@gmail.com>
2019-03-28 09:13:49 +0100

    Update add-woocomm-dynamic.php
    Added type selector

--- add-woocomm-dynamic.php
+++ add-woocomm-dynamic.php
@@ -41,6 +41,17 @@ $add_woocomm_dynamic->add_field(
     )
 );
 
+$add_woocomm_dynamic->add_field(
+	'applies_to',
+	'Applies To',
+    'radio',
+    array(
+        'everyone' => 'Everyone'
+    )
+);
+
+$add_woocomm_dynamic->add_field('type', 'Type', 'radio', array('price_discount' => 'Price discount', 'percentage_discount' => 'Percentage discount', 'fixed_price' => 'Fixed price'));
+
 $add_woocomm_dynamic->add_field('date_from', 'Date From', 'text');
 $add_woocomm_dynamic->add_field('date_to', 'Date To', 'text');
 
@@ -55,4 +66,3 @@ function add_woocomm_dynamic_function() {
 }
 
 $add_woocomm_dynamic->run();
-
